ed41f1c95c7025cc32696005e39ad250b84432a7
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-06-20 09:53:48 -0700

    Updated classification function to use fine-tuning.
    Former-commit-id: 68250043dd32e85b9d06aa7554e66141c969e0b7

--- R/classifySingleR.R
+++ R/classifySingleR.R
@@ -37,7 +37,10 @@
 #' @importFrom methods is
 #' @importClassesFrom SingleCellExperiment SingleCellExperiment
 #' @importFrom SummarizedExperiment colData<- colData assay
-classifySingleR <- function(x, trained, threshold=0.2, assay.type=1, BNPARAM=KmknnParam()) {
+#' @importFrom BiocParallel SerialParam
+classifySingleR <- function(x, trained, quant.thresh=0.2, fine.tune=TRUE, tune.thresh=0.05, assay.type=1, 
+    BNPARAM=KmknnParam(), BPPARAM=SerialParam()) 
+{
     if (is.null(rownames(x))) {
         stop("'x' must have row names")
     }
@@ -46,25 +49,36 @@ classifySingleR <- function(x, trained, threshold=0.2, assay.type=1, BNPARAM=Kmk
         x <- assay(x, i=assay.type)
     }
 
-    ref.genes <- rownames(trained$original)
+    ref.genes <- trained$common.genes
     if (all(ref.genes %in% rownames(x))) {
         x <- x[ref.genes,,drop=FALSE]
     } else {
         stop("'rownames(x)' does not contain all genes used in 'trained'")
     }
 
+    # Initial search in rank space.
     ranked <- scaledColRanks(x, transposed=TRUE)
-    all.indices <- trained$indices
+    all.indices <- trained$nn.indices
     scores <- matrix(0, nrow(ranked), length(all.indices), dimnames=list(rownames(ranked), names(all.indices)))
-    for (u in names(trained$indices)) {
-        curdex <- trained$indices[[u]]
-        k <- round(nrow(curdex) * threshold)
+    for (u in names(all.indices)) {
+        curdex <- all.indices[[u]]
+        k <- max(1, round(nrow(curdex) * quant.thresh))
         nn.out <- queryKNN(query=ranked, k=k, BNINDEX=curdex, get.index=FALSE)
         scores[,u] <- 1 - 2*nn.out$distance[,ncol(nn.out$distance)]^2 # see https://arxiv.org/abs/1208.3145
     }
 
+    # Fine-tuning with an iterative search in lower dimensions.
     labels <- colnames(scores)[max.col(scores)]
-    output <- DataFrame(scores=I(scores), labels=labels)
+    if (fine.tune) {
+        new.labels <- .fine_tune_data(exprs=x, scores=scores, genes=trained$search.mode, 
+            extras=trained$extra, references=trained$original.exprs, 
+            quant.thresh=quant.thresh, tune.thresh=tune.thresh, 
+            sd.thresh=trained$sd.thresh, BPPARAM=BPPARAM)
+        output <- DataFrame(scores=I(scores), labels=new.labels, first.labels=labels)
+    } else {
+        output <- DataFrame(scores=I(scores), labels=labels)
+    }
+
     if (was.sce) {
         colData(original)$SingleR <- output
         return(original)
