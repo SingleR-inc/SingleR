e59cdcd710ad43134f83ba92b2d92b68cb2d90cb
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-07-05 17:24:14 -0700

    Reorganized fine-tuning tests.
    Former-commit-id: 7ce7f028e2bc423d6930f0c19cb68b58b2abf8da

--- tests/testthat/test-fine-tune.R
+++ tests/testthat/test-fine-tune.R
@@ -1,23 +1,94 @@
 # This tests the fine-tuning functionality.
 # library(testthat); library(SingleR); source("setup.R"); source("test-fine-tune.R")
 
+# A reference implementation in pure R.
+.fine_tune_cell <- function(i, exprs, scores, references, quantile, tune.thresh, commonFUN, ...) {
+    cur.exprs <- exprs[,i]
+    cur.scores <- scores[i,]
+    top.labels <- names(cur.scores)[cur.scores >= max(cur.scores) - tune.thresh]
+    old.labels <- character(0)
+
+    # Need to compare to old.labels, to avoid an infinite loop 
+    # if the correlations are still close after fine tuning.
+    while (length(top.labels) > 1L && !identical(top.labels, old.labels)) {
+        common <- commonFUN(top.labels, ...)
+        cur.scores <- .compute_label_scores_manual(common, top.labels, cur.exprs, references, quantile=quantile)
+        old.labels <- top.labels
+        cur.scores <- cur.scores[!is.na(cur.scores)]
+        top.labels <- names(cur.scores)[cur.scores >= max(cur.scores) - tune.thresh] 
+    }
+
+    if (length(top.labels)==1L) {
+        top.labels
+    } else if (length(top.labels)==0L) {
+        NA_character_
+    } else {
+        names(cur.scores)[which.max(cur.scores)]
+    }
+}
+
+.compute_label_scores_manual <- function(common, top.labels, cur.exprs, references, quantile) {
+    cur.exprs <- cur.exprs[common]
+    cur.scores <- numeric(length(top.labels))
+    names(cur.scores) <- top.labels
+
+    for (u in top.labels) {
+        ref <- references[[u]]
+        ref <- as.matrix(ref[common,,drop=FALSE])
+
+        # We a 'k'-based method for selecting the quantile, for consistency with classifySingleR.
+        k <- max(1, round((1-quantile) * ncol(ref)))
+        cur.cor <- cor(cur.exprs, ref, method="spearman")
+        cur.scores[u] <- -sort(-cur.cor, partial=k)[k]
+    }
+    cur.scores
+}
+
+.fine_tune_de_genes <- function(top.labels, de.info) {
+    # Finding the subset of genes (assuming 'extras' is a matrix of lists).
+    all.combos <- expand.grid(top.labels, top.labels)
+    Reduce(union, de.info[as.matrix(all.combos)])
+}
+
+.fine_tune_sd_genes <- function(top.labels, extras, sd.thresh, sd.n=500) {
+    sds <- DelayedMatrixStats::rowSds(extras, col=match(top.labels, colnames(extras)))
+    sd.n <- min(length(sds), sd.n)
+    sd.thresh <- min(sd.thresh, -sort(-sds, partial=sd.n)[sd.n])
+    rownames(extras)[sds > sd.thresh]
+}
+
+.fine_tune_de <- function(exprs, scores, references, quantile, tune.thresh, de.info, BPPARAM=SerialParam()) {
+    de.info <- do.call(cbind, de.info)
+    if (is.null(colnames(de.info)) || !identical(colnames(de.info), rownames(de.info))) {
+        stop("marker list should be named during training")
+    }
+    out <- lapply(seq_len(ncol(exprs)), FUN=.fine_tune_cell, exprs=exprs, scores=scores, 
+        references=references, quantile=quantile, tune.thresh=tune.thresh, 
+        commonFUN=.fine_tune_de_genes, de.info=de.info, BPPARAM=BPPARAM)            
+    unlist(out)
+}
+
+############################
+############################
+# Checking that our test functions are actually correct. 
+
 test_that("fine-tuning correlation calculator is consistent with classifySingleR", {
     trained <- trainSingleR(training, training$label, genes='all')
 
     Q <- 0.8
-    out <- SingleR:::.compute_label_scores_manual(assay(test)[,1], trained$original.exprs, 
+    out <- .compute_label_scores_manual(assay(test)[,1], trained$original.exprs, 
         top.labels=names(trained$original.exprs), common=rownames(test), quantile=Q)
     ref <- classifySingleR(test, trained, fine.tune=FALSE, quantile=Q)
     expect_equal(out, ref$scores[1,])
 
     Q <- 0.2
-    out <- SingleR:::.compute_label_scores_manual(assay(test)[,2], trained$original.exprs,
+    out <- .compute_label_scores_manual(assay(test)[,2], trained$original.exprs,
         top.labels=names(trained$original.exprs), common=rownames(test), quantile=Q)
     ref <- classifySingleR(test, trained, fine.tune=FALSE, quantile=Q)
     expect_equal(out, ref$scores[2,])
 
     Q <- 0.5
-    out <- SingleR:::.compute_label_scores_manual(assay(test)[,3], trained$original.exprs, 
+    out <- .compute_label_scores_manual(assay(test)[,3], trained$original.exprs, 
         top.labels=names(trained$original.exprs), common=rownames(test), quantile=Q)
     ref <- classifySingleR(test, trained, fine.tune=FALSE, quantile=Q)
     expect_equal(out, ref$scores[3,])
@@ -27,14 +98,52 @@ test_that("fine-tuning correlation calculator responds to gene subsets", {
     trained <- trainSingleR(training, training$label, genes='de')
 
     Q <- 0.8
-    out <- SingleR:::.compute_label_scores_manual(assay(test)[,1], trained$original.exprs, 
+    out <- .compute_label_scores_manual(assay(test)[,1], trained$original.exprs, 
         common=trained$common.genes, top.labels=names(trained$original.exprs), quantile=Q)
     ref <- classifySingleR(test, trained, fine.tune=FALSE, quantile=Q)
     expect_equal(out, ref$scores[1,])
 
-    out <- SingleR:::.compute_label_scores_manual(assay(test)[,2], trained$original.exprs, 
+    out <- .compute_label_scores_manual(assay(test)[,2], trained$original.exprs, 
         common=trained$common.genes, top.labels=names(trained$original.exprs)[1:2], quantile=Q)
     expect_equal(out, ref$scores[2,1:2])
+})
+
+test_that("fine-tuning DE marker selection works", {
+    trained <- trainSingleR(training, training$label, genes='de')
+
+    stuff <- as.list(trained$search$extra)
+    de.info <- do.call(cbind, stuff)
+    out <- .fine_tune_de_genes(c("A", "B"), de.info)
+    expect_identical(sort(out), sort(union(stuff$A$B, stuff$B$A)))
+
+    # Throwing if the constructed matrix doesn't have dimnames.
+    Q <- 0.8
+    thresh <- 0.05
+    expect_error(.fine_tune_de(assay(test), ref$scores, trained$original.exprs,
+         quantile=Q, tune.thresh=thresh, de.info=unname(trained$search$extra)), "named")
+    expect_error(.fine_tune_de(assay(test), ref$scores, trained$original.exprs,
+         quantile=Q, tune.thresh=thresh, de.info=lapply(trained$search$extra, unname)), "named")
+})
+
+test_that("fine-tuning SD selection works", {
+    trained <- trainSingleR(training, training$label, genes='sd')
+
+    mat <- trained$search$extra
+    lab <- c("A", "B", "C")
+    ref <- DelayedMatrixStats::rowSds(mat[,lab])
+
+    # Responds to various settings correctly
+    # (difficult to test exactly without regurgitating the code).
+    out <- .fine_tune_sd_genes(lab, mat, 1, sd.n=50)
+    expect_true(min(ref[match(out, rownames(mat))]) > max(ref[-match(out, rownames(mat))]))
+
+    out2 <- .fine_tune_sd_genes(lab, mat, 1, sd.n=100)
+    expect_true(length(out2) > length(out))
+    expect_true(min(ref[match(out2, rownames(mat))]) > max(ref[-match(out2, rownames(mat))]))
+
+    out3 <- .fine_tune_sd_genes(lab, mat, 0.5, sd.n=50)
+    expect_true(length(out3) > length(out))
+    expect_true(min(ref[match(out3, rownames(mat))]) > max(ref[-match(out3, rownames(mat))]))
 })
 
 ###################################
@@ -46,75 +155,49 @@ training$label <- paste0(training$label, subset)
 
 test_that("fine-tuning by DE runs without errors", {
     trained <- trainSingleR(training, training$label, genes='de')
-    ref <- classifySingleR(test, trained, fine.tune=FALSE)
+    pred <- classifySingleR(test, trained, fine.tune=FALSE)
 
+    for (Q in c(0, 0.2, 0.5, 0.8, 1)) {
+        for (thresh in c(0, 0.05, 0.1)) {
+            tuned <- SingleR:::.fine_tune_de(assay(test), pred$scores, trained$original.exprs, 
+                 quantile=Q, tune.thresh=thresh, de.info=trained$search$extra)
+            ref <- .fine_tune_de(assay(test), pred$scores, trained$original.exprs, 
+                 quantile=Q, tune.thresh=thresh, de.info=trained$search$extra)
+
+            expect_equal(colnames(pred$scores)[tuned+1], ref)
+        }
+    }
+
+    # Sanity checking of the dimensions and output.
     Q <- 0.8
     thresh <- 0.05
     tuned <- SingleR:::.fine_tune_de(assay(test), ref$scores, trained$original.exprs, 
          quantile=Q, tune.thresh=thresh, de.info=trained$search$extra)
 
-    expect_identical(length(tuned), nrow(ref))
-    is.diff <- tuned!=ref$labels
+    expect_identical(length(tuned), nrow(pred))
+    is.diff <- tuned!=pred$labels
     expect_true(any(is.diff))
 
-    maxed <- ref$scores[cbind(seq_len(nrow(ref)), max.col(ref$scores))]
-    expect_false(any(is.diff & rowSums(ref$scores >= maxed - thresh)==1L))
+    maxed <- pred$scores[cbind(seq_len(nrow(pred)), max.col(pred$scores))]
+    expect_false(any(is.diff & rowSums(pred$scores >= maxed - thresh)==1L))
 })
 
-test_that("fine-tuning DE marker selection works", {
-    trained <- trainSingleR(training, training$label, genes='de')
-
-    stuff <- as.list(trained$search$extra)
-    de.info <- do.call(cbind, stuff)
-    out <- SingleR:::.fine_tune_de_genes(c("A1", "B2"), de.info)
-    expect_identical(sort(out), sort(union(stuff$A1$B2, stuff$B2$A1)))
-
-    # Throwing if the constructed matrix doesn't have dimnames.
-    Q <- 0.8
-    thresh <- 0.05
-    expect_error(SingleR:::.fine_tune_de(assay(test), ref$scores, trained$original.exprs, 
-         quantile=Q, tune.thresh=thresh, de.info=unname(trained$search$extra)), "named")
-    expect_error(SingleR:::.fine_tune_de(assay(test), ref$scores, trained$original.exprs, 
-         quantile=Q, tune.thresh=thresh, de.info=lapply(trained$search$extra, unname)), "named")
-})
-
-test_that("fine-tuning by SD runs without errors", {
-    trained <- trainSingleR(training, training$label, genes='sd')
-    ref <- classifySingleR(test, trained, fine.tune=FALSE)
-
-    Q <- 0.8
-    thresh <- 0.05
-    tuned <- SingleR:::.fine_tune_sd(assay(test), ref$scores, trained$original.exprs, 
-         quantile=Q, tune.thresh=thresh, median.mat=trained$search$extra, sd.thresh=1)
-
-    expect_identical(length(tuned), nrow(ref))
-    is.diff <- tuned!=ref$labels
-    expect_true(any(is.diff))
-
-    maxed <- ref$scores[cbind(seq_len(nrow(ref)), max.col(ref$scores))]
-    expect_false(any(is.diff & rowSums(ref$scores >= maxed - thresh)==1L))
-})
-
-test_that("fine-tuning SD selection works", {
-    trained <- trainSingleR(training, training$label, genes='sd')
-
-    mat <- trained$search$extra
-    lab <- c("A1", "A2", "B1", "B2")
-    ref <- DelayedMatrixStats::rowSds(mat[,lab])
-
-    # Responds to various settings correctly
-    # (difficult to test exactly without regurgitating the code).
-    out <- SingleR:::.fine_tune_sd_genes(lab, mat, 1, sd.n=100)
-    expect_true(min(ref[match(out, rownames(mat))]) > max(ref[-match(out, rownames(mat))]))
-
-    out2 <- SingleR:::.fine_tune_sd_genes(lab, mat, 1, sd.n=200)
-    expect_true(length(out2) > length(out))
-    expect_true(min(ref[match(out2, rownames(mat))]) > max(ref[-match(out2, rownames(mat))]))
-
-    out3 <- SingleR:::.fine_tune_sd_genes(lab, mat, 0.5, sd.n=100)
-    expect_true(length(out3) > length(out))
-    expect_true(min(ref[match(out3, rownames(mat))]) > max(ref[-match(out3, rownames(mat))]))
-})
+#test_that("fine-tuning by SD runs without errors", {
+#    trained <- trainSingleR(training, training$label, genes='sd')
+#    ref <- classifySingleR(test, trained, fine.tune=FALSE)
+#
+#    Q <- 0.8
+#    thresh <- 0.05
+#    tuned <- SingleR:::.fine_tune_sd(assay(test), ref$scores, trained$original.exprs, 
+#         quantile=Q, tune.thresh=thresh, median.mat=trained$search$extra, sd.thresh=1)
+#
+#    expect_identical(length(tuned), nrow(ref))
+#    is.diff <- tuned!=ref$labels
+#    expect_true(any(is.diff))
+#
+#    maxed <- ref$scores[cbind(seq_len(nrow(ref)), max.col(ref$scores))]
+#    expect_false(any(is.diff & rowSums(ref$scores >= maxed - thresh)==1L))
+#})
 
 ###################################
 ###################################
