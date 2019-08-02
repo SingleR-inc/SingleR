3b400f32a3a41e543eaca73bda8115bf121fc454
Jared Andrews <jared.andrews07@gmail.com>
2019-08-02 16:15:00 -0500

    Add getSinglerRef function for temporary retrieval of reference datasets

---
+++ R/helper.R
@@ -0,0 +1,44 @@
+#' Retrieve SingleR reference datasets from Github.
+#'
+#' \code{getSinglerRef} retrieves \code{SingleR} reference datasets from Github. 
+#'
+#' @param dataset String referring to SingleR reference dataset to retrieve. 
+#'     Options include:
+#'     \itemize{
+#'       \item "hpca"
+#'       \item "blueprint_encode"
+#'       \item "immgen"
+#'       \item "mouse.rnaseq"
+#'     }
+#' @return SingleR reference dataset.
+#'
+#' @author Jared Andrews
+#'
+#' @examples
+#'
+#' immgen <- getSinglerRef(dataset = "immgen")
+#' mouse.rnaseq <- getSinglerRef(dataset = "mouse.rnaseq")
+#'
+#' @export
+#' @importFrom curl curl_download
+#' 
+getSinglerRef <- function(dataset = "hpca") {
+	  opts <- c("hpca", "blueprint_encode", "immgen", "mouse.rnaseq")
+	  if (!(dataset %in% opts)) {
+		    stop("Invalid SingleR reference set. Valid options are 'hpca',", 
+			      "'blueprint_encode', 'immgen', or 'mouse.rnaseq'.")
+	  }
+	  base.url <- "https://github.com/dviraran/SingleR/blob/master/data/"
+	  url.tail <- ".rda?raw=true"
+
+	  full.url <- paste0(base.url, dataset, url.tail)
+
+	  bfc <- BiocFileCache(ask=FALSE)
+	  ref <- bfcrpath(bfc, full.url)
+
+	  # This could be less gross if rds files were used instead.
+	  load(ref, envir = environment())
+	  ref.set <- get(dataset, envir = environment())
+	
+	  return(ref.set)
+}
