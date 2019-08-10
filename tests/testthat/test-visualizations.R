9eef37afc71a73c1d5f956b725ec116e6edcb3dc
Daniel Bunis <daniel.bunis@ucsf.edu>
2019-08-10 16:42:51 -0700

    Update test-visualizations.R
    Changed the code to use the setup.R generated objects exclusively, instead of example(SingleR)
    Placed the "We can pass excess pheatmap::pheatmap parameters through plotScoreHeatmap." plot function in an expect statement.
    Moved the `font.size` adjustment into the main plotScoreHeatmap function because it is just another input like all the others.
    Removed the commented code for potentially comparing plots because it is not necessary.

--- tests/testthat/test-visualizations.R
+++ tests/testthat/test-visualizations.R
@@ -1,8 +1,10 @@
 # Tests for visualization functions
 # library(SingleR); library(testthat); source("setup.R"); source("test-visualization.R")
 
+colnames(test) <- paste0("name",seq_len(ncol(counts(test))))
 test.exp <- assay(test,1)
 training.exp <- assay(training,1)
+pred <- SingleR(test=test.exp, training=training.exp, labels=training$label, genes="de")
 
 test_that("we can produce expression comparison scatterplots with plotCellVsReference", {
   expect_error(plotCellVsReference(test,1,training,1), NULL) # looks for logcounts, but the dataset does not have logcounts
@@ -16,10 +18,6 @@ test_that("we can produce expression comparison scatterplots with plotCellVsRefe
                                 P3 <- plotCellVsReference(test, 1, training, 2, 1, 1))))
   expect_false(isTRUE(all.equal(P2, P3)))
 })
-
-example("SingleR")
-colnames(test) <- paste0("name",seq_len(ncol(counts(test))))
-pred <- SingleR(test, sce, labels=sce$label)
 
 test_that("We can produce heatmaps of scores with plotScoreHeatmap", {
   expect_s3_class(plotScoreHeatmap(results = pred),
@@ -52,41 +50,13 @@ test_that("We can produce heatmaps of scores with plotScoreHeatmap", {
   expect_s3_class(plotScoreHeatmap(results = pred,
                                    silent=TRUE),
                   "pheatmap")
-})
-
-test_that("Explicit visualization adjustments don't break plotScoreHeatmap plotting.",{
   expect_s3_class(plotScoreHeatmap(results = pred,
                                    fontsize.row = 5),
                   "pheatmap")
 })
 
 test_that("We can pass excess pheatmap::pheatmap parameters through plotScoreHeatmap.", {
-  plotScoreHeatmap(results = pred,
-                   cutree_col = 3)
+  expect_s3_class(plotScoreHeatmap(results = pred,
+                                   cutree_col = 3),
+                  "pheatmap")
 })
-
-# I'm not sure how best to compare two plots (ggplot nor pheatmap).
-# test_that("Providing indices versus cell names in plotScoreHeatmap 'cells.use' does the same thing",{
-#   hm1 <- plotScoreHeatmap(results = pred,
-#                           cells.use = 1:50)
-#   hm2 <- plotScoreHeatmap(results = pred,
-#                           cells.use = rownames(pred)[1:50])
-#   # These outputs of these two plots, made above, should be the same, but some names underneath the hood are different.
-#   # expect_equal(hm1, hm2) #This fails because it checks unplotted info.
-# })
-# test_that("When order.by.clusters = TRUE, cells.order is overrided in plotScoreHeatmap 'cells.use'",{
-#   hm1 <- plotScoreHeatmap(results = pred,
-#                           clusters = pred$labels,
-#                           order.by.clusters=TRUE)
-#   hm2 <- plotScoreHeatmap(results = pred,
-#                           clusters = pred$labels,
-#                           cells.order=seq_len(nrow(pred)), #not utilized because order.by.clusters takes precedence
-#                           order.by.clusters=TRUE)
-#   hm3 <- plotScoreHeatmap(results = pred,
-#                           clusters = pred$labels,
-#                           cells.order=seq_len(nrow(pred)),
-#                           order.by.clusters=FALSE)
-#   # I'm not sure how to do this! The info inside a pheatmap is hard to parse and probably not great to access anyway.
-#   # hm1 and hm2 shoul dbe the same plot
-#   # hm3 should be a different plot
-# })
