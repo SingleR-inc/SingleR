62a86dbbdbea22b36aea5afb5dd33eeae7e62b08
Daniel Bunis <daniel.bunis@ucsf.edu>
2019-08-13 12:17:30 -0700

    Fixed 'wonky formatting'
    Moved all arguments to a new line, rather than having just a few in the opening bracket line, when any arguments were on a second line.
    Reformatted all second-line-code to be single-tabbed instead of aligned to starting parentheses.

--- tests/testthat/test-visualizations.R
+++ tests/testthat/test-visualizations.R
@@ -7,29 +7,37 @@ test.exp <- assay(test,1)
 training.exp <- assay(training,1)
 
 test_that("we can produce expression comparison scatterplots with plotCellVsReference", {
-  expect_error(plotCellVsReference(test = test,1,
-                                   ref = training,1,
-                                   5, 5), NULL)
-  expect_s3_class(plotCellVsReference(test = test, 1,
-                                      ref = training, 1,
-                                      1, 1), "ggplot")
-  expect_s3_class(P <- plotCellVsReference(test = test, 1,
-                                           ref = training, 1), "ggplot")
-  expect_s3_class(plotCellVsReference(test = test.exp, 1,
-                                      ref = training.exp, 1), "ggplot")
-  expect_s3_class(plotCellVsReference(test = test, 1,
-                                      ref = training.exp, 1,
-                                      1,5), "ggplot")
-  expect_s3_class(plotCellVsReference(test = test.exp, 1,
-                                      ref = training, 1,
-                                      5,1), "ggplot")
-  expect_false(isTRUE(all.equal(P,
-                                P2 <- plotCellVsReference(test = test, 2,
-                                                          ref = training, 1))))
-  expect_false(isTRUE(all.equal(P, 
-                                P3 <- plotCellVsReference(test = test, 1,
-                                                          ref = training, 2))))
-  expect_false(isTRUE(all.equal(P2, P3)))
+    expect_error(plotCellVsReference(
+        test = test,1,
+        ref = training,1,
+        5, 5), NULL)
+    expect_s3_class(plotCellVsReference(
+        test = test, 1,
+        ref = training, 1,
+        1, 1), "ggplot")
+    expect_s3_class(P <- plotCellVsReference(
+        test = test, 1,
+        ref = training, 1), "ggplot")
+    expect_s3_class(plotCellVsReference(
+        test = test.exp, 1,
+        ref = training.exp, 1), "ggplot")
+    expect_s3_class(plotCellVsReference(
+        test = test, 1,
+        ref = training.exp, 1,
+        1,5), "ggplot")
+    expect_s3_class(plotCellVsReference(
+        test = test.exp, 1,
+        ref = training, 1,
+        5,1), "ggplot")
+    expect_false(isTRUE(all.equal(
+        P,
+        P2 <- plotCellVsReference(test = test, 2,
+        ref = training, 1))))
+    expect_false(isTRUE(all.equal(
+        P, 
+        P3 <- plotCellVsReference(test = test, 1,
+        ref = training, 2))))
+    expect_false(isTRUE(all.equal(P2, P3)))
 })
 
 test_that("We can produce heatmaps of scores with plotScoreHeatmap", {
