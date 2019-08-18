72d4f5a570e4bd3c51f0b8fb7b288c09ada4082d
Daniel Bunis <Daniel.Bunis@ucsf.edu>
2019-08-17 22:58:00 -0700

    Update pruneScores.R
    - Shorten the min.diff argument names to min.diff.med and min.diff.next.
    - Set min.diff.next's default to zero.

--- R/pruneScores.R
+++ R/pruneScores.R
@@ -3,8 +3,8 @@
 #' Remove low-quality assignments based on the cell-label score matrix returned by \code{\link{classifySingleR}}.
 #'
 #' @param scores SingleR output, or a Matrix of scores per cell (row) and label (column) generated during assignment by \code{\link{classifySingleR}}.
-#' @param min.diff.vs.median Numeric scalar specifying the minimum difference of each cell's maximum score from the median score.
-#' @param min.percent.diff.vs.next Numeric scalar specifying the minimum percent difference between the best score and the next best score.
+#' @param min.diff.med Numeric scalar specifying the minimum difference of each cell's maximum score from the median score.
+#' @param min.diff.next Numeric scalar specifying the minimum percent difference between the best score and the next best score.
 #' @param nmads Numeric scalar specifying the number of MADs to use to define cells with low outlier scores per label.
 #' 
 #' @return A logical vector specifying which assignments should be ignored.
@@ -16,11 +16,12 @@
 #' The \code{pruneScores} function aims to mitigate this effect by removing poor-quality assignments with \dQuote{low} scores.
 #'
 #' We define a low score using two orthogonal measures, per-cell and per-label.
-#' For the per-cell check, we see whether the maximum score is less than \code{min.diff} above the median score for each cell.
+#' For the per-cell check, we see whether the maximum score is less than \code{min.diff.med} above the median score for each cell.
 #' If so, this indicates that the cell matches all labels with the same confidence 
 #' such that the one reported label is not particularly meaningful.
 #' This can also be justified in high-dimensional analyses where,
 #' in the absence of any strong similarity to a single label, all distances converge to the same value.
+#' 
 #'  
 #' For the per-label check, we identify cells that are small outliers in the distribution of scores for each label.
 #' (This only includes cells that pass the per-cell check.)
@@ -30,18 +31,18 @@
 #' Thus, small outliers represent poor-quality assignments from a distinct subpopulation that should be removed.
 #'
 #' The defaults for both of these parameters are largely arbitrary and chosen based on experience.
-#' Smaller values for \code{min.diff} and larger values for \code{nmads} will reduce the stringency of the pruning.
+#' Smaller values for \code{min.diff.med} and larger values for \code{nmads} will reduce the stringency of the pruning.
 #' In particular:
 #' \itemize{
 #' \item The scores do not consider the effects of fine-tuning (as scores are not comparable across different fine-tuning steps).
 #' In situations involving a majority of labels with only subtle distinctions,
 #' it is possible for the scores to be relatively similar but for the labels to be correctly assigned after fine-tuning.
-#' In such cases, the default setting of \code{min.diff} may be too stringent.
+#' In such cases, the default setting of \code{min.diff.med} may be too stringent.
 #' \item It is possible for the per-label score distribution to be multimodal yet still correct,
 #' e.g., due to cells belong to subtypes when the (correct) label corresponds to the main type.
 #' In such cases, the default \code{nmads} may be too stringent as it will remove minor subpopulations with low scores.
 #' }
-#' Note that decreasing \code{min.diff} may actually \emph{increase} the stringency of the per-label check, 
+#' Note that decreasing \code{min.diff.med} may actually \emph{increase} the stringency of the per-label check, 
 #' depending on whether the additional retained cells decrease the MAD.
 #'
 #' @author Aaron Lun
@@ -56,22 +57,22 @@
 #' summary(pruneScores(pred$scores))
 #'
 #' # Less stringent:
-#' summary(pruneScores(pred$scores, min.diff=0))
+#' summary(pruneScores(pred$scores, min.diff.med=0))
 #' summary(pruneScores(pred$scores, nmads=5))
 #' 
 #' @export
 #' @importFrom DelayedMatrixStats rowMedians 
 #' @importFrom DelayedArray DelayedArray rowMaxs
 #' @importFrom stats median mad
-pruneScores <- function(scores, min.diff.vs.median = 0.05, nmads=3, min.percent.diff.vs.next = 0.05) {
+pruneScores <- function(scores, min.diff.med = 0.05, nmads=3, min.diff.next = 0) {
     if (is(scores, "DataFrame")){
       scores <- scores$scores
     }
 
     maxed <- rowMaxs(DelayedArray(scores))
     delta <- maxed - rowMedians(DelayedArray(scores))
-    keep <- delta >= min.diff.vs.median
-    keep <- keep & (rowSums(scores/maxed > (1-min.percent.diff.vs.next)) < 2)
+    keep <- delta >= min.diff.med
+    keep <- keep & (rowSums(scores/maxed > (1-min.diff.next)) < 2)
 
     best <- max.col(scores)
     by.label <- split(which(keep), best[keep])
