3107ff7ebe9e5ad07cf8d2a030d0588ab24ebd12
Jared Andrews <jared.andrews07@gmail.com>
2019-08-28 17:45:00 -0500

    Add data wrangling for MonacoImmuneData directly from GEO.

--- inst/scripts/make-monaco_immune-metadata.R
+++ inst/scripts/make-monaco_immune-metadata.R
@@ -1,19 +1,19 @@
 df <- data.frame(
-    Title = sprintf("DMAP RNA-seq %s", c("logcounts", "colData")), # This can be the exact file name (if self-describing) or a more complete description.
+    Title = sprintf("Monaco Immune Cell RNA-seq %s", c("logcounts", "colData")), # This can be the exact file name (if self-describing) or a more complete description.
     Description = sprintf("%s 114 RNA-seq samples of immune cells from GSE107011", 
         c("Matrix of log-normalized expression values from", "Per-sample metadata containing cell type labels of")),
     RDataPath = file.path("SingleR", "monaco_immune","1.0.0", c("logcounts.rds", "coldata.rds")),
     BiocVersion="3.10", # The first Bioconductor version the resource was made available for.
     Genome=NA, # Can be NA.
-    SourceType="RDA", #  Format of original data, e.g., FASTA, BAM, BigWig, etc. ‘getValidSourceTypes()’ for currently acceptable values
-    SourceUrl="https://www.dropbox.com/sh/z6uig95fm37whus/AAAGMA7VtNh_BblJppmakZ98a?dl=0",
-    SourceVersion="monaco_immune.rda",
+    SourceType="TXT", #  Format of original data, e.g., FASTA, BAM, BigWig, etc. ‘getValidSourceTypes()’ for currently acceptable values
+    SourceUrl="https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE107011",
+    SourceVersion="1.0.0",
     Species="Homo sapiens", # getSpeciesList, validSpecies, or suggestSpecies(); can be NA
     TaxonomyId="9606",
     Coordinate_1_based=NA, #TRUE, FALSE, NA
     DataProvider="GEO",
     Maintainer="Jared Andrews <jared.andrews@wustl.edu>",
-    RDataClass="character", # R / Bioconductor class the data are stored in, e.g., GRanges, SummarizedExperiment, ExpressionSet etc.
+    RDataClass="data.frame", # R / Bioconductor class the data are stored in, e.g., GRanges, SummarizedExperiment, ExpressionSet etc.
     DispatchClass="Rds", # Determines how data are loaded into R.
     stringsAsFactors = FALSE
 )
