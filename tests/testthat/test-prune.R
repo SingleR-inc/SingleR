5230955b4c0e50fc206de43442b1365a9ffa1cd5
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2019-08-31 21:33:06 -0700

    Pruning now uses outlier detection on delta values.

--- tests/testthat/test-prune.R
+++ tests/testthat/test-prune.R
@@ -12,7 +12,7 @@ test_that("validating per-cell check without finetuning", {
     colnames(scores) <- LETTERS[1:5]
     
     results <- DataFrame(scores=I(scores))
-    expect_identical(pruneScores(results), c(FALSE, FALSE, TRUE, TRUE, TRUE))
+    expect_identical(pruneScores(results, min.diff.med=0.05), c(FALSE, FALSE, TRUE, TRUE, TRUE))
     expect_identical(pruneScores(results, min.diff.med=2), !logical(5))
 })
 
@@ -38,4 +38,10 @@ test_that("validating per-label check", {
     results <- DataFrame(scores=I(copies))
     expect_identical(pruneScores(results, nmads=3), rowSums(copies) < 0.5)
     expect_identical(pruneScores(results, nmads=10000), logical(nrow(copies)))
+
+    # Uses deltas, not the raw scores.
+    copies <- rbind(scores, scores*0.9+0.1, scores*0.8+0.2, scores*0.1+0.9)
+    results2 <- DataFrame(scores=I(copies))
+    expect_identical(pruneScores(results, nmads=3), pruneScores(results2, nmads=3))
+    expect_identical(pruneScores(results, nmads=1000), pruneScores(results2, nmads=1000))
 })
