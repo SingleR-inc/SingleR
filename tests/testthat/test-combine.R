89c3a98e07e621e7a76dc6cf10c1ed10b24adec7
Jared Andrews <jared.andrews07@gmail.com>
2019-12-03 03:11:39 -0600

    Add basic tests

---
+++ tests/testthat/test-combine.R
@@ -0,0 +1,71 @@
+# This tests that combineResults works as expected.
+# library(testthat); library(SingleR); source("test-combine.R")
+
+test_that("combineResults works as expected", {
+    scores <- rbind(
+        c(0,0,0,0,1),
+        c(0,0,0,2,0),
+        c(0,0,1,0,0),
+        c(0,2,0,0,0),
+        c(1,0,0,0,0)
+    )
+    colnames(scores) <- LETTERS[1:5]
+    
+    labs <- colnames(scores)[max.col(scores)]
+    results <- DataFrame(scores=I(scores), labels=labs, pruned.labels=labs)
+    rownames(results) <- sprintf("GENE_%s", seq_len(nrow(results)))
+
+    scores2 <- rbind(
+        c(0,0,0,0,2),
+        c(0,0,0,1,0),
+        c(0,0,2,0,0),
+        c(0,1,0,0,0),
+        c(2,0,0,0,0)
+    )
+    colnames(scores2) <- LETTERS[6:10]
+    
+    labs2 <- colnames(scores2)[max.col(scores2)]
+    results2 <- DataFrame(scores=I(scores2), labels=labs2, pruned.labels=labs2)
+    rownames(results2) <- sprintf("GENE_%s", seq_len(nrow(results2)))
+
+    combined <- combineResults(list(res1=results, res2=results2))
+    expect_identical(combined$scores, cbind(scores, scores2))
+    expect_identical(combined$labels, c("J", "D", "H", "B", "F"))
+    expect_identical(combined$pruned.labels, c("J", "D", "H", "B", "F"))
+    expect_equivalent(combined$orig.results$res1, results)
+    expect_equivalent(combined$orig.results$res2, results2)
+})
+
+test_that("combineResults handles non-matching cells", {
+    scores <- diag(5)
+    colnames(scores) <- LETTERS[1:5]
+    
+    results <- DataFrame(scores=I(scores), labels=colnames(scores)[max.col(scores)])
+    rownames(results) <- c("this", "is", "a", "neat", "test")
+
+    scores2 <- diag(5)
+    colnames(scores2) <- LETTERS[6:10]
+    
+    results2 <- DataFrame(scores=I(scores2), labels=colnames(scores2)[max.col(scores2)])
+    rownames(results2) <- c("this", "is", "a", "neat", "package")
+
+    expect_error(combined <- combineResults(list(res1=results, res2=results2)), "cell/cluster names are not identical")
+})
+
+test_that("combineResults handles non-common genes", {
+    scores <- diag(5)
+    colnames(scores) <- LETTERS[1:5]
+    
+    results <- DataFrame(scores=I(scores), labels=colnames(scores)[max.col(scores)])
+    metadata(results)$common.genes <- c("i", "am", "a", "common", "gene")
+    rownames(results) <- sprintf("GENE_%s", seq_len(nrow(results)))
+
+    scores2 <- diag(5)
+    colnames(scores2) <- LETTERS[6:10]
+    
+    results2 <- DataFrame(scores=I(scores2), labels=colnames(scores2)[max.col(scores2)])
+    metadata(results2)$common.genes <- c("i", "am", "an", "uncommon", "gene")
+    rownames(results2) <- sprintf("GENE_%s", seq_len(nrow(results2)))
+
+    expect_warning(combined <- combineResults(list(res1=results, res2=results2)), "common genes are not identical")
+})
