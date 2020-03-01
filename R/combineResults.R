0d9ea35a2c0360013b9293255146c10a872eec3c
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-02-29 16:57:02 -0800

    Added draft of the recomputing reference combiner.

--- R/combineResults.R
+++ R/combineResults.R
@@ -36,6 +36,7 @@
 #' @section Option 3 - comparing scores across the union of markers:
 #' This option involves performing classification separately within each reference, then collating the results to choose the label with the highest score across references. 
 #' This is a relatively expedient approach that avoids the need for explicit harmonization while also reduces the potential for reference-specific markers.
+#' It is also logistically simpler as it allows each reference to be processed separately (more or less, depending on the exact algorithm) for embarrassing parallelization.
 #'
 #' It leaves a mixture of labels in the final results that is up to the user to resolve, though perhaps this may be considered a feature as it smoothly handles differences in resolution between references, e.g., a cell that cannot be resolved as a CD4+ or CD8+ T cell may simply fall back to \code{"T cell"}.
 #' It will also be somewhat suboptimal if there are many reference-specific labels, as markers are not identified with the aim of distinguishing a label in one reference from another label in another reference.
@@ -49,7 +50,7 @@
 #' \code{\link{matchReferences}}, to harmonize labels between reference datasets.
 NULL
 
-#' Combine SingleR results
+#' Combine SingleR results with common genes
 #'
 #' Combine results from multiple runs of \code{\link{classifySingleR}} (usually against different references) into a single \linkS4class{DataFrame}.
 #' The label from the results with the highest score for each cell is retained.
@@ -228,4 +229,206 @@ combineCommonResults <- function(results) {
     output$orig.results <- do.call(DataFrame, lapply(results, I))
 
     output
+}
+
+#' Combine SingleR results with recomputation
+#'
+#' Combine results from multiple runs of \code{\link{classifySingleR}} (usually against different references) into a single \linkS4class{DataFrame}.
+#' The label from the results with the highest score for each cell is retained.
+#' Unlike \code{\link{combineCommonResults}}, this does not assume that each run of \code{\link{classifySingleR}} was performed using the same set of common genes, instead recomputing the scores for comparison across references.
+#'
+#' @param results A list of \linkS4class{DataFrame} prediction results as returned by \code{\link{classifySingleR}} when run on each reference separately.
+#' @inheritParams SingleR
+#' @param ref A list or \linkS4class{List} of the same length as \code{results}.
+#' Each entry can be a SummarizedExperiment object or numeric matrix containing the reference used to obtain the annotations in the corresponding entry of \code{results}.
+#' See the equivalent argument in \code{\link{trainSingleR}} for details.
+#' @param labels A list of the same length as \code{ref},
+#' where each element should contain a character vector or factor specifying the label for the corresponding entry of \code{ref}.
+#'
+#' @return A \linkS4class{DataFrame} is returned containing combined annotations for each cell or cluster (row).
+#' This has the following fields:
+#' \itemize{
+#' \item \code{labels}, a character vector containing the best label chosen across all references.
+#' \item \code{reference}, an integer vector specifying the reference result from which the best label originates.
+#' \item \code{inter.delta.med}, a numeric vector containing the delta value across references.
+#' \item \code{intra.delta.med}, a numeric vector containing the delta value within the originating reference.
+#' }
+#'
+#' @details
+#' This function implements a variant of Option 3 described in \code{?\link{combine-predictions}}).
+#' For a given cell in \code{test}, we extract its assigned label from \code{results} for each reference.
+#' We also retrieve the marker genes associated with that label and take the union of markers across all references.
+#' This defines a common feature space in which the score for each reference's assigned label is recomputed using \code{ref};
+#' the label from the reference with the top recomputed score is then reported as the combined annotation for that cell.
+#' 
+#' Unlike \code{\link{combineCommonResults}}, the union of markers is not used for the within-reference calls.
+#' This avoids the inclusion of noise from irrelevant genes in the within-reference assignments.
+#' Obviously, \code{combineRecomputedResults} is slower as it does require recomputation of the scores,
+#' but the within-reference calls are faster as there are fewer genes in the union of markers for assigned labels
+#' (compared to the union of markers across all labels, as required by \code{\link{combineCommonResults}}),
+#' so it is likely that the net compute time should be lower.
+#'
+#' This function is intended for use by end-users and requires inputs similar to those of multi-reference calls to the \code{\link{SingleR}} function.
+#' No modification of the output of \code{\link{trainSingleR}} is required as there is no need for a set of common genes during the within-reference classifications.
+#' 
+#' @author Aaron Lun
+#'
+#' @examples
+#' ##############################
+#' ## Mocking up training data ##
+#' ##############################
+#'
+#' Ngroups <- 5
+#' Ngenes <- 1000
+#' means <- matrix(rnorm(Ngenes*Ngroups), nrow=Ngenes)
+#' means[1:900,] <- 0
+#' colnames(means) <- LETTERS[1:5]
+#'
+#' g <- rep(LETTERS[1:5], each=4)
+#' g2 <- rep(LETTERS[6:10], each=4)
+#' ref1 <- SummarizedExperiment(
+#'     list(counts=matrix(rpois(1000*length(g), 
+#'     lambda=10*2^means[,g]), ncol=length(g))),
+#'     colData=DataFrame(label=g)
+#' )
+#' ref2 <- SummarizedExperiment(
+#'     list(counts=matrix(rpois(1000*length(g2), 
+#'     lambda=10*2^means[,g]), ncol=length(g2))),
+#'     colData=DataFrame(label=g2)
+#' )
+#' rownames(ref1) <- sprintf("GENE_%s", seq_len(nrow(ref1)))
+#' rownames(ref2) <- sprintf("GENE_%s", seq_len(nrow(ref2)))
+#'
+#' ref1 <- scater::logNormCounts(ref1)
+#' ref2 <- scater::logNormCounts(ref2)
+#'
+#' ###############################
+#' ## Mocking up some test data ##
+#' ###############################
+#'
+#' N <- 100
+#' g <- sample(LETTERS[1:5], N, replace=TRUE)
+#' means <- matrix(rnorm(Ngenes*Ngroups), nrow=Ngenes)
+#' means[1:900] <- 0
+#' colnames(means) <- LETTERS[1:5]
+#' test <- SummarizedExperiment(
+#'     list(counts=matrix(rpois(1000*N, lambda=2^means[,g]), ncol=N)),
+#'     colData=DataFrame(label=g)
+#' )
+#' 
+#' rownames(test) <- sprintf("GENE_%s", seq_len(nrow(test)))
+#' test <- scater::logNormCounts(test)
+#' 
+#' ###############################
+#' ## Performing classification ##
+#' ###############################
+#' 
+#' pred1 <- SingleR(test, ref1, labels=ref1$label)
+#' pred2 <- SingleR(test, ref2, labels=ref2$label)
+#'
+#' combined <- combineSeparateResults(list(pred1, pred2), test=test,
+#'     ref=list(ref1, ref2), labels=list(ref1$label, ref2$label))
+#' head(combined)
+#'
+#' @export
+#' @importFrom S4Vectors DataFrame selfmatch metadata
+#' @importFrom BiocParallel bpiterate SerialParam
+#' @importFrom BiocNeighbors KmknnParam
+combineRecomputedResults <- function(results, test, ref, labels, quantile=0.8, 
+    assay.type.test="logcounts", assay.type.ref="logcounts", check.missing=TRUE,
+    BNPARAM=KmknnParam(), BPPARAM=SerialParam())
+{
+    collated <- markers <- commons <- vector("list", length(results))
+    for (x in seq_along(results)) {
+        current <- results[[x]]
+        collated[[x]] <- current$labels
+        markers[[x]] <- metadata(current)$de.genes
+        commons[[x]] <- metadata(current)$common.genes
+    }
+
+    names(collated) <- make.names(seq_along(collated))
+    collated <- DataFrame(collated)
+    groups <- selfmatch(collated)
+    by.group <- split(seq_along(groups), groups)
+
+    test <- .to_clean_matrix(test, assay.type=assay.type.test, check.missing=check.missing, msg="test")
+    ref <- lapply(ref, FUN=.to_clean_matrix, assay.type=assay.type.ref, check.missing=check.missing, msg="ref")
+
+    available <- rownames(test)
+    for (r in ref) { 
+        available <- intersect(available, rownames(r)) 
+    }
+
+    # Using iterator to avoid having to serialize the entire object to the workers.
+    # Rather, only the data for the relevant markers for the current labels is serialized.
+    envir <- new.env()
+    envir$counter <- 1L
+    ITER <- function() {
+        if (envir$counter > length(by.group)) {
+            return(NULL)
+        }
+        curgroup <- by.group[[envir$counter]]
+        curlabels <- collated[curgroup[1],,drop=FALSE]
+        envir$counter <- envir$counter + 1L
+
+        curmarkers <- character(0)
+        for (i in seq_along(markers)) {
+            if (is.null(markers[[i]])) {
+                to.add <- commons[[i]]
+            } else {
+                curlab <- curlabels[[i]]
+                to.add <- markers[[i]][[curlab]]
+                to.add <- unlist(to.add, use.names=FALSE)
+            }
+            curmarkers <- union(curmarkers, to.add)
+        }
+
+        curmarkers <- intersect(curmarkers, available)
+        all.ref <- vector("list", length(ref))
+        for (i in seq_along(ref)) {
+            keep <- curlabels[[i]]==labels[[i]]
+            all.ref[[i]] <- ref[[i]][curmarkers,keep,drop=FALSE]
+        }
+
+        list(
+            test=test[curmarkers,curgroup,drop=FALSE],
+            ref=do.call(cbind, all.ref),
+            label=rep(seq_along(all.ref), vapply(all.ref, ncol, 0L)),
+            names=unlist(curlabels)
+        )
+    }
+
+    combined <- bpiterate(
+        ITER=ITER,
+        FUN=function(data, BNPARAM, quantile, ...) {
+            test <- data$test
+            ref <- data$ref
+            label <- data$label
+            names <- data$names
+
+            # We have no interest looking for marker genes between references,
+            # as this would leave us susceptible to batch effects. So, don't
+            # bother to detect genes or to fine-tune, we just want the scores
+            # and the identity of the label that delivers the maximum score.
+            trained <- trainSingleR(ref, label, genes = "all", 
+                check.missing = FALSE, BNPARAM = BNPARAM)
+            output <- classifySingleR(test, trained, quantile = quantile, fine.tune = FALSE, 
+                prune = FALSE, check.missing = FALSE)
+   
+            ref.idx <- as.integer(output$labels)
+            DataFrame(
+                reference=ref.idx,
+                labels=names[ref.idx],
+                inter.delta.med=getDeltaFromMedian(output)
+            )
+        },
+        BPPARAM=BPPARAM,
+        quantile=quantile,
+        BNPARAM=BNPARAM
+    )
+
+    combined <- do.call(rbind, combined)
+    combined <- combined[order(unlist(by.group)),,drop=FALSE]
+
+    combined
 }
