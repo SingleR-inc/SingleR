c1d97aa57d17eaebe51c71402afa39b7c93bf4d7
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-03-04 23:31:24 -0800

    First round of bugfixes to proposed fine-tuning approach.

--- R/fine_tune.R
+++ R/fine_tune.R
@@ -2,37 +2,43 @@
 #' @importFrom BiocParallel SerialParam bpiterate
 #' @importFrom S4Vectors DataFrame
 #' @importFrom BiocNeighbors KmknnParam
+#' @importFrom DelayedArray rowMaxs
 .fine_tune_loop <- function(exprs, scores, references, 
     quantile, tune.thresh, defineFUN, 
     BPPARAM=SerialParam(), BNPARAM=KmknnParam())
 {
     indices <- list()
     present <- DataFrame(scores[0,])
-    final <- character(nrow(scores))
     all.labels <- colnames(scores)
+
+    final <- character(nrow(scores))
     survivors <- seq_len(nrow(scores))
+    remaining <- exprs
+    last.nhits <- integer(nrow(scores))
 
     repeat {
-        max.score <- scores[cbind(seq_len(nrow(scores)), max.col(scores))]
-        limit <- max.score - tune.thresh
-        above <- scores >= limit
-        encoding <- DataFrame(overlord)
-        nhits <- Reduce("+", encoding)
-        finished <- nhits==1L
+        above <- scores >= rowMaxs(scores) - tune.thresh
+        nhits <- rowSums(above)
+        finished <- nhits==1L | nhits==last.nhits[survivors]
+        last.nhits[survivors] <- nhits
 
         final[survivors[finished]] <- "X" # TODO: fix this.
-        survivors <- survivors[!finished]
-        encoding <- encoding[!finished,,drop=FALSE]
-        if (length(survivors)==0L) {
-            break
+
+        if (all(finished)) {
+           break
+        } else if (any(finished)) {
+            survivors <- survivors[!finished]
+            above <- above[!finished,,drop=FALSE]
+            remaining <- remaining[,!finished,drop=FALSE]
         }
 
-        i.out <- .build_indices(encoding, present=present, indices=indices, defineFUN=defineFUN, BNPARAM=BNPARAM)
+        i.out <- .build_indices(above, present=present, indices=indices, 
+            references=references, defineFUN=defineFUN, BNPARAM=BNPARAM)
         present <- i.out$present
         indices <- i.out$indices
         use <- i.out$use
 
-        ITER <- .fine_tune_iterator(use, present=present, indices=indices, exprs=exprs) 
+        ITER <- .fine_tune_iterator(use, present=present, indices=indices, exprs=remaining)
         output <- bpiterate(ITER=ITER, FUN=.fine_tune_searcher, BPPARAM=BPPARAM, quantile=quantile, all.labels=all.labels)
         scores <- do.call(rbind, output)
         scores[order(use),] <- scores # need to reorder as iterator splits up by 'use'.
@@ -42,17 +48,21 @@
 }
 
 #' @importFrom BiocNeighbors buildIndex KmknnParam
-#' @importFrom S4Vectors selfmatch
-.build_indices <- function(encoding, present, indices, defineFUN, BNPARAM=KmknnParam()) {
-    smatched <- selfmatch(encoding)
-    representatives <- encoding[!duplicated(matched),,drop=FALSE]
+#' @importFrom S4Vectors DataFrame selfmatch
+#' @importFrom BiocGenerics match
+.build_indices <- function(encoding, present, indices, references, defineFUN, BNPARAM=KmknnParam()) {
+    encoding2 <- DataFrame(encoding)
+    smatched <- selfmatch(encoding2)
+    smatched <- as.integer(factor(smatched))
+
+    representatives <- encoding2[!duplicated(smatched),,drop=FALSE]
     imatched <- match(representatives, present)
     absent <- which(is.na(imatched))
 
     absent.indices <- vector("list", length(absent))
     for (i in seq_along(absent)) {
-        chosen <- representatives[absent[i],]
-        chosen <- colnames(scores)[unlist(chosen)]
+        chosen <- encoding[absent[i],]
+        chosen <- colnames(encoding)[chosen]
         common <- defineFUN(chosen)
 
         idxs <- lapply(references[chosen], function(x) {
@@ -62,9 +72,9 @@
         absent.indices[[i]] <- list(trained=idxs, genes=common)
     }
 
+    imatched[absent] <- length(indices) + seq_along(absent.indices)
     indices <- c(indices, absent.indices)
     present <- rbind(present, representatives[absent,])
-    imatched[absent] <- length(indices) + seq_along(absent.indices)
 
     list(
         present=present,
@@ -100,81 +110,43 @@
     ranked <- .scaled_colranks_safe(exprs)
     output <- lapply(trained, FUN=.find_nearest_quantile, ranked=ranked, quantile=quantile)
 
-    overlord <- matrix(-Inf, nrow=nrow(ranked), ncol=length(all.labels),
+    overlord <- matrix(-Inf, 
+        nrow=nrow(ranked), ncol=length(all.labels),
         dimnames=list(rownames(ranked), all.labels))
+
     for (i in names(output)) {
         overlord[,i] <- output[[i]]
     }
     overlord
 }
 
-# COMMENTS FROM AARON:
-# We decline to use nearest neighbors here, because there's no opportunity to build a single index.
-# Moreover, the data are so reduced in both dimensions that the algorithmic complexity is likely
-# offset by the reduction in overhead when just computing the correlations directly.
-# One could possibly improve vectorization by grouping together test cells with the same
-# combination of topLabels, but it adds a lot of complexity and additional overhead.
-
-#' @importFrom BiocParallel bplapply bpmapply SerialParam
-.fine_tune_de <- function(exprs, scores, references, quantile, tune.thresh, de.info, BPPARAM=SerialParam()) {
-    # Converting character vectors into integer indices.
-    # We assume that SingleR() has already set up the backend.
-    de.info <- bplapply(de.info, function(markers, genes, labels) {
-        for (j in seq_along(markers)) {
-            markers[[j]] <- match(markers[[j]], genes) - 1L
-        }
-        markers[labels]
-    }, genes=rownames(exprs), labels=colnames(scores), BPPARAM=BPPARAM)
-    de.info <- de.info[colnames(scores)]
-
-    M <- .prep_for_parallel(exprs, BPPARAM)
-    S <- .prep_for_parallel(t(scores), BPPARAM)
-    bp.out <- bpmapply(Exprs=M, scores=S, FUN=fine_tune_label_de, 
-        MoreArgs=list(References=references, quantile=quantile, tune_thresh=tune.thresh, marker_genes=de.info), 
-        BPPARAM=BPPARAM, SIMPLIFY=FALSE, USE.NAMES=FALSE)
-
-    do.call(mapply, c(bp.out, list(FUN=c, SIMPLIFY=FALSE, USE.NAMES=FALSE)))
-}
-
-#' @importFrom BiocParallel bpmapply SerialParam
-.fine_tune_sd <- function(exprs, scores, references, quantile, tune.thresh, median.mat, sd.thresh, BPPARAM=SerialParam()) {
-    M <- .prep_for_parallel(exprs, BPPARAM)
-    S <- .prep_for_parallel(t(scores), BPPARAM)
-    bp.out <- bpmapply(Exprs=M, scores=S, FUN=fine_tune_label_sd, 
-        MoreArgs=list(References=references, quantile=quantile, tune_thresh=tune.thresh, median_mat=t(median.mat), sd_thresh=sd.thresh),
-        BPPARAM=BPPARAM, SIMPLIFY=FALSE, USE.NAMES=FALSE)
-
-    do.call(mapply, c(bp.out, list(FUN=c, SIMPLIFY=FALSE, USE.NAMES=FALSE)))
-}
-
-#' @importFrom BiocParallel bpnworkers
-.prep_for_parallel <- function(mat, BPPARAM) {
-    is.int <- !is.double(mat[0,])
-    n_cores <- bpnworkers(BPPARAM)
-
-    if (n_cores==1L) {
-        # Can't be bothered to template it twice at the C++ level,
-        # as we'd have to have both int/numeric versions for the test and reference.
-        if (is.int) {
-            mat <- mat + 0
-        }
-        return(list(mat))
+#' @importFrom BiocParallel SerialParam
+#' @importFrom BiocNeighbors KmknnParam
+.fine_tune_de <- function(exprs, scores, references, quantile, tune.thresh, de.info, 
+    BPPARAM=SerialParam(), BNPARAM=KmknnParam()) 
+{
+    deFUN <- function(labels) {
+        targets <- lapply(de.info[labels], function(y) unique(unlist(y[labels], use.names=FALSE)))
+        Reduce(union, targets)
     }
 
-    # Split the matrix *before* parallelization,
-    # otherwise the full matrix gets serialized to all workers.
-    boundaries <- as.integer(seq(from = 1L, to = ncol(mat)+1L, length.out = n_cores + 1L)) 
-    out <- vector("list", n_cores)
+    .fine_tune_loop(exprs, scores=scores, references=references,
+        quantile=quantile, tune.thresh=tune.thresh, defineFUN=deFUN,
+        BPPARAM=BPPARAM, BNPARAM=BNPARAM)
+}
 
-    for (i in seq_along(out)) {
-        cur_start <- boundaries[i]
-        cur_end <- boundaries[i+1]
-        curmat <- mat[,(cur_start - 1L) + seq_len(cur_end - cur_start),drop=FALSE]
-        if (is.int) {
-            curmat <- curmat + 0
-        }
-        out[[i]] <- curmat
+#' @importFrom BiocParallel SerialParam
+#' @importFrom BiocNeighbors KmknnParam
+#' @importFrom DelayedMatrixStats rowSds
+.fine_tune_sd <- function(exprs, scores, references, quantile, tune.thresh, median.mat, sd.thresh, 
+    BPPARAM=SerialParam(), BNPARAM=KmknnParam()) 
+{
+    sdFUN <- function(labels) {
+        curmat <- median.mat[,labels,drop=FALSE]
+        rownames(median.mat)[rowSds(mat) >= sd.thresh]
     }
 
-    out
+    .fine_tune_loop(exprs, scores=scores, references=references,
+        quantile=quantile, tune.thresh=tune.thresh, defineFUN=sdFUN,
+        BPPARAM=BPPARAM, BNPARAM=BNPARAM)
 }
