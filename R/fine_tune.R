30bf9e3acf970af3eafbdd0f56a0abfb58132832
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-03-04 22:15:49 -0800

    Initial draft of refactoring to use indexed fine-tuning.

--- R/fine_tune.R
+++ R/fine_tune.R
@@ -1,3 +1,113 @@
+#' @importFrom BiocGenerics rbind 
+#' @importFrom BiocParallel SerialParam bpiterate
+#' @importFrom S4Vectors DataFrame
+#' @importFrom BiocNeighbors KmknnParam
+.fine_tune_loop <- function(exprs, scores, references, 
+    quantile, tune.thresh, defineFUN, 
+    BPPARAM=SerialParam(), BNPARAM=KmknnParam())
+{
+    indices <- list()
+    present <- DataFrame(scores[0,])
+    final <- character(nrow(scores))
+    all.labels <- colnames(scores)
+    survivors <- seq_len(nrow(scores))
+
+    repeat {
+        max.score <- scores[cbind(seq_len(nrow(scores)), max.col(scores))]
+        limit <- max.score - tune.thresh
+        above <- scores >= limit
+        encoding <- DataFrame(overlord)
+        nhits <- Reduce("+", encoding)
+        finished <- nhits==1L
+
+        final[survivors[finished]] <- "X" # TODO: fix this.
+        survivors <- survivors[!finished]
+        encoding <- encoding[!finished,,drop=FALSE]
+        if (length(survivors)==0L) {
+            break
+        }
+
+        i.out <- .build_indices(encoding, present=present, indices=indices, defineFUN=defineFUN, BNPARAM=BNPARAM)
+        present <- i.out$present
+        indices <- i.out$indices
+        use <- i.out$use
+
+        ITER <- .fine_tune_iterator(use, present=present, indices=indices, exprs=exprs) 
+        output <- bpiterate(ITER=ITER, FUN=.fine_tune_searcher, BPPARAM=BPPARAM, quantile=quantile, all.labels=all.labels)
+        scores <- do.call(rbind, output)
+        scores[order(use),] <- scores # need to reorder as iterator splits up by 'use'.
+    }
+
+    final
+}
+
+#' @importFrom BiocNeighbors buildIndex KmknnParam
+#' @importFrom S4Vectors selfmatch
+.build_indices <- function(encoding, present, indices, defineFUN, BNPARAM=KmknnParam()) {
+    smatched <- selfmatch(encoding)
+    representatives <- encoding[!duplicated(matched),,drop=FALSE]
+    imatched <- match(representatives, present)
+    absent <- which(is.na(imatched))
+
+    absent.indices <- vector("list", length(absent))
+    for (i in seq_along(absent)) {
+        chosen <- representatives[absent[i],]
+        chosen <- colnames(scores)[unlist(chosen)]
+        common <- defineFUN(chosen)
+
+        idxs <- lapply(references[chosen], function(x) {
+            rout <- .scaled_colranks_safe(x[common,,drop=FALSE])
+            buildIndex(rout, BNPARAM=BNPARAM)
+        })
+        absent.indices[[i]] <- list(trained=idxs, genes=common)
+    }
+
+    indices <- c(indices, absent.indices)
+    present <- rbind(present, representatives[absent,])
+    imatched[absent] <- length(indices) + seq_along(absent.indices)
+
+    list(
+        present=present,
+        indices=indices,
+        use=imatched[smatched]
+    )
+}
+
+.fine_tune_iterator <- function(use, present, indices, exprs) {
+    u.use <- sort(unique(use))
+    counter <- 1L
+
+    function() {
+        if (counter > length(u.use)) {
+            return(NULL)
+        }
+
+        in.use <- u.use[counter]
+        used <- use==in.use
+        curdex <- indices[[in.use]]
+
+        counter <<- counter + 1L
+        list(
+            exprs=exprs[curdex$genes,used,drop=FALSE],
+            trained=curdex$trained
+        )
+    }
+}
+
+.fine_tune_searcher <- function(data, quantile, all.labels) {
+    exprs <- data$exprs
+    trained <- data$trained
+    ranked <- .scaled_colranks_safe(exprs)
+    output <- lapply(trained, FUN=.find_nearest_quantile, ranked=ranked, quantile=quantile)
+
+    overlord <- matrix(-Inf, nrow=nrow(ranked), ncol=length(all.labels),
+        dimnames=list(rownames(ranked), all.labels))
+    for (i in names(output)) {
+        overlord[,i] <- output[[i]]
+    }
+    overlord
+}
+
 # COMMENTS FROM AARON:
 # We decline to use nearest neighbors here, because there's no opportunity to build a single index.
 # Moreover, the data are so reduced in both dimensions that the algorithmic complexity is likely
