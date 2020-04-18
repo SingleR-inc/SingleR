3f4c51cb181ab3b1c9122a3b976a3ba6cc7fdc5b
dtm2451 <Daniel.Bunis@ucsf.edu>
2020-04-17 17:17:43 -0700

    shift utils-Viz.R into utils.R

--- R/utils.R
+++ R/utils.R
@@ -21,3 +21,18 @@
 
     x
 }
+
+.grab_results <- function(results, index) {
+    if (index == 0 || is.null(results$orig.results)) {
+        return(results)
+    } else {
+        return(results$orig.results[[index]])
+    }
+}
+
+.ensure_named <- function(results) {
+    if (is.null(rownames(results))) {
+        rownames(results) <- seq_len(nrow(results))
+    }
+    results
+}
