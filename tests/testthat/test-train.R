82d619048a50e3d49b129cb2536629e8b8139a11
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2026-02-23 17:09:16 +1100

    Fix.

--- tests/testthat/test-train.R
+++ tests/testthat/test-train.R
@@ -220,9 +220,8 @@ test_that("trainSingleR behaves with aggregation turned on", {
 test_that("trainSingleR behaves with silly inputs", {
     expect_error(out <- trainSingleR(training[,0], training$label[0]), "at least one column")
 
-    # R itself is buggy right now w.r.t. handling dimnames when the extent is zero.
-    #out <- trainSingleR(training[0,], training$label)
-    #expect_identical(length(out$markers$unique), 0L)
+    out <- trainSingleR(training[0,], training$label)
+    expect_identical(length(out$markers$unique), 0L)
 
     unnamed <- unname(training)
     expect_error(trainSingleR(unnamed, unnamed$label), "must have row names")
